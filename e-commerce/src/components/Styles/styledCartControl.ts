73e587079b50737260fa0df5c726b84ad9cd97c2
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-04 21:37:31 -0300

    feat: implement style cartcontrol

---
+++ e-commerce/src/components/Styles/styledCartControl.ts
@@ -0,0 +1,18 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+    position: relative;
+`;
+
+export const CartCount = styled.span`
+    width: 17px;
+    height: 17px;
+    background-color: var(--delete-color);
+    border-radius: 100%;
+    padding: 0 5px;
+    margin-left: -10px;
+    font-size: 12px;
+    color: #fff;
+   
+`;
+
