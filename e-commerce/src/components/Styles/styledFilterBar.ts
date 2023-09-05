2895d55a2b7a2e991396edcb4243e3367562733a
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-05 20:56:00 -0300

    feat: implement styled Priority

--- e-commerce/src/components/Styles/styledFilterBar.ts
+++ e-commerce/src/components/Styles/styledFilterBar.ts
@@ -29,4 +29,56 @@ export const FilterContainer = styled.div`
   display: flex;
   width: 100%;
   align-items: start;
+  justify-content: space-between;
+`;
+
+export const FilterContainerPripority = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+    font-family: inherit;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    color: var(--text-dark);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      margin-left: 16px;
+    }
+  }
+`;
+
+export const PriorityFilter = styled.ul`
+  position: absolute;
+  background-color: #ffff;
+  border-radius: 4px;
+  box-shadow: 0px 4px 12px rgba(0,0,0,0.1);
+  padding: 12px 16px;
+  list-style: none;
+  width: 250px;
+  top: 100%;
+
+  li {
+    color: var(--text-dark);
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 22px;
+    cursor: pointer;
+
+    &:hover{
+      border-bottom: 0.5px solid var(--orange-low);
+    }
+  }
+
+  li + li {
+    margin-top: 4px;
+  }
 `;
