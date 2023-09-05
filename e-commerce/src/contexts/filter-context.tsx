7ff3213d18e5c144a9a561201300d1bad3c0aef7
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-05 20:55:06 -0300

    feat: add priorityTypes

--- e-commerce/src/contexts/filter-context.tsx
+++ e-commerce/src/contexts/filter-context.tsx
@@ -1,15 +1,18 @@
-"use client"
+"use client";
 import { FilterType } from "@/types/filter-types";
+import { PriorityTypes } from "@/types/prioruty-types";
 import { ReactNode, createContext, useState } from "react";
 
 interface ProviderProps {
   children: ReactNode;
 }
 
- export const FilterContext = createContext({
+export const FilterContext = createContext({
   search: "",
   page: 0,
   type: FilterType.ALL,
+  priority: PriorityTypes.NEWS,
+  setPriority: (value: PriorityTypes) => {},
   setSearch: (value: string) => {},
   setPage: (value: number) => {},
   setType: (value: FilterType) => {},
@@ -19,10 +22,20 @@ export function FilterContextProvider({ children }: ProviderProps) {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(0);
   const [type, setType] = useState(FilterType.ALL);
+  const [priority, setPriority] = useState(PriorityTypes.NEWS);
 
   return (
     <FilterContext.Provider
-      value={{ search, page, type, setSearch, setPage, setType }}
+      value={{
+        search,
+        page,
+        type,
+        setSearch,
+        setPage,
+        setType,
+        priority,
+        setPriority,
+      }}
     >
       {children}
     </FilterContext.Provider>
