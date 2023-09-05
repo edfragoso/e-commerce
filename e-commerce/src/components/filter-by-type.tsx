6cd592ceaeef63dbe9b96008fec3bbc8c6dfaa70
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-05 19:45:16 -0300

    feat: state control

--- e-commerce/src/components/filter-by-type.tsx
+++ e-commerce/src/components/filter-by-type.tsx
@@ -1,12 +1,35 @@
-"use client"
+"use client";
+import { useFilter } from "@/hooks/useFilter";
 import { FilterItem, FilterList } from "./Styles/styledFilterBar";
+import { FilterType } from "@/types/filter-types";
 
 export function FilterByType() {
+  const { type, setType } = useFilter();
+
+  const handleChangeType = (value: FilterType) => {
+    setType(value);
+  };
+
   return (
     <FilterList>
-      <FilterItem selected={true}>Todos os produtos</FilterItem>
-      <FilterItem selected={false}>Camisetas</FilterItem>
-      <FilterItem selected={false}>Canecas</FilterItem>
+      <FilterItem
+        selected={type === FilterType.ALL}
+        onClick={() => handleChangeType(FilterType.ALL)}
+      >
+        Todos os produtos
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.SHIRT}
+        onClick={() => handleChangeType(FilterType.SHIRT)}
+      >
+        Camisetas
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.MUG}
+        onClick={() => handleChangeType(FilterType.MUG)}
+      >
+        Canecas
+      </FilterItem>
     </FilterList>
   );
 }
