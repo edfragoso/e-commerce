1ddbdb2d1873ff0d237b779021629ebb9df7f256
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-05 20:56:53 -0300

    feat: add FilterByPriority

--- e-commerce/src/components/filterBar.tsx
+++ e-commerce/src/components/filterBar.tsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 import { FilterContainer } from "./Styles/styledFilterBar";
+import { FilterByPriority } from "./filter-by-priority";
 import { FilterByType } from "./filter-by-type";
 
 export function FilterBar() {
   return (
     <FilterContainer>
       <FilterByType />
+      <FilterByPriority />
     </FilterContainer>
   );
 }
