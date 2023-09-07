e8cc0c309c4b69d92df11751fcdabed84a620aec
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-07 00:50:56 -0300

    feat: implement ProductCar

--- e-commerce/src/components/productLis.tsx
+++ e-commerce/src/components/productLis.tsx
@@ -1,10 +1,21 @@
-"use client"
-import { useProducts } from "@/hooks/useProducts"
+"use client";
+import { useProducts } from "@/hooks/useProducts";
+import { ProductCard } from "./ProductCard";
+import { ListContainer } from "./Styles/styledProducCard";
 
 export function ProductList() {
-    const { products } = useProducts();
-    console.log(products)
-    return(
-        <></>
-    )
+  const { products } = useProducts();
+  /* console.log(products) */
+  return (
+    <ListContainer>
+      {products?.map((product) => (
+        <ProductCard
+          key={product.id}
+          title={product.name}
+          price={product.price_in_cents}
+          image={product.image_url}
+        />
+      ))}
+    </ListContainer>
+  );
 }
