ea629ffde0d2f96a8bbaaec2d01957bf4432ad6e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-06 23:06:59 -0300

    feat: implement QueryClientProvider

--- e-commerce/src/app/page.tsx
+++ e-commerce/src/app/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image'
+"use client"
 import styles from './page.module.css'
 import { FilterBar } from '@/components/filterBar'
+import { ProductList } from '@/components/productLis'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 export default function Home() {
+  const client = new QueryClient()
   return (
+    <QueryClientProvider client={client}>
     <main className={styles.main}>
       <FilterBar />
+      <ProductList />
     </main>
+    </QueryClientProvider>
   )
 }
