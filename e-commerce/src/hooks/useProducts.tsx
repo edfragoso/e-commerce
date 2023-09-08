64ed3607b5c230154d9ae71be56a5d9e21afa7d5
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-08 13:10:20 -0300

    refactor: implement filter

--- e-commerce/src/hooks/useProducts.tsx
+++ e-commerce/src/hooks/useProducts.tsx
@@ -2,32 +2,32 @@
 import { ProductsFetchResponse } from "@/types/productsResponse";
 import axios, { AxiosPromise } from "axios";
 import { useQuery } from "react-query";
+import { useFilter } from "./useFilter";
+import { useDeferredValue } from "react";
+import { mountQuery } from "@/utils/filtersGraphql";
 
-/* const apiURL = process.env.NEXT_PUBLIC_API_URL as string  */
+/* const apiURL = process.env.NEXT_PUBLIC_API_URL as string   */
 
-const fetcher = (): AxiosPromise<ProductsFetchResponse> => {
-  return axios.post("http://localhost:3333", {
-    query: `
-        query {
-            allProducts {
-                id
-                name
-                price_in_cents
-                image_url
-            }
-        }`,
-  });
+const fetcher = (query: string): AxiosPromise<ProductsFetchResponse> => {
+  return axios.post("http://localhost:3333", { query });
 };
 
 export function useProducts() {
+  const { type, priority, search } = useFilter();
+  const searchDeferred = useDeferredValue(search);
+  const query = mountQuery(type, priority);
   const { data } = useQuery({
-    queryFn: fetcher,
-    queryKey: ["product"],
+    queryFn: () => fetcher(query),
+    queryKey: ["products", type, priority],
   });
-  
-  const products = data?.data.data.allProducts;
+
+  const products = data?.data?.data?.allProducts;
+  const filteredProducts = products?.filter((product) =>
+    product.name.toLowerCase().includes(searchDeferred.toLowerCase())
+  );
   console.log(products);
+  console.log(filteredProducts);
   return {
-    products,
+    data: filteredProducts,
   };
 }
