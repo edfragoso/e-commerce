c15c6484547d22b5508aa42db10b46c4e08721bc
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-08 13:13:15 -0300

    feat: add event filter

--- e-commerce/src/components/primary-input.tsx
+++ e-commerce/src/components/primary-input.tsx
@@ -2,12 +2,18 @@ import { InputHTMLAttributes } from "react";
 import { InputContainer, PrimaryInput } from "./Styles/styledPrimary-input";
 import { SearchIcon } from "./icons/search-icon";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  value: string;
+  handleChange: (value: string) => void;
+}
 
 export function PrimaryInputSearchIcon(props: InputProps) {
   return (
     <InputContainer>
-      <PrimaryInput {...props} />
+      <PrimaryInput
+        onChange={(event) => props.handleChange(event.target.value)}
+        {...props}
+      />
       <SearchIcon />
     </InputContainer>
   );
