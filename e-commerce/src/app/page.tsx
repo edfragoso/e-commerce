45c4dbad8a174e753d51f49683771cf97896fa11
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-11 13:49:10 -0300

    refactor: extract providers

--- e-commerce/src/app/page.tsx
+++ e-commerce/src/app/page.tsx
@@ -1,17 +1,14 @@
-"use client"
-import styles from './page.module.css'
-import { FilterBar } from '@/components/filterBar'
-import { ProductList } from '@/components/productLis'
-import { QueryClient, QueryClientProvider } from 'react-query'
+"use client";
+import styles from "./page.module.css";
+import { FilterBar } from "@/components/filterBar";
+import { ProductList } from "@/components/productLis";
 
 export default function Home() {
-  const client = new QueryClient()
+  
   return (
-    <QueryClientProvider client={client}>
     <main className={styles.main}>
       <FilterBar />
       <ProductList />
     </main>
-    </QueryClientProvider>
-  )
+  );
 }
