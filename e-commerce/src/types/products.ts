67a74ff9d5a4060771d92ccf7176e2ec76c01e62
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-11 21:49:42 -0300

    feat: add interface ProductFetchResponse

--- e-commerce/src/types/products.ts
+++ e-commerce/src/types/products.ts
@@ -3,4 +3,12 @@ export interface Product {
     name: string,
     image_url: string,
     price_in_cents: number,
+    description?: string,
+    category?: string,
+}
+
+export interface ProductFetchResponse {
+    data: {
+        Product: Product
+    }
 }
