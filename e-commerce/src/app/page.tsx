efcfd8df202f77f4a9b3122cae0209593982cb59
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-11 21:58:17 -0300

    refactor: style page home

--- e-commerce/src/app/page.tsx
+++ e-commerce/src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
-import styles from "./page.module.css";
+
 import { FilterBar } from "@/components/filterBar";
 import { ProductList } from "@/components/productLis";
+import { DefaultPageLayout } from "@/components/Styles/styledDefaultPageLayout";
 import { PageWrapper } from "@/components/Styles/styledPageMain";
 
 export default function Home() {
-  
   return (
-    <PageWrapper>
-      <FilterBar />
-      <ProductList />
-    </PageWrapper>
+    <DefaultPageLayout>
+      <PageWrapper>
+        <FilterBar />
+        <ProductList />
+      </PageWrapper>
+    </DefaultPageLayout>
   );
 }
