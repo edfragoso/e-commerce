ec9805073bc2822d61f3a149d4842b2fa47c16aa
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-11 21:51:39 -0300

    feat: implement style Button

---
+++ e-commerce/src/components/Styles/styledBackButton.ts
@@ -0,0 +1,16 @@
+"use client"
+import styled from "styled-components";
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap:8px;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 150%;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  color: var(--secondary-text);
+`;
