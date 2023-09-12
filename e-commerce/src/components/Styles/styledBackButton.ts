67931cb250905541802764e5cb86b752a3d4af44
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-12 11:09:27 -0300

    feat: add params styles

--- e-commerce/src/components/Styles/styledBackButton.ts
+++ e-commerce/src/components/Styles/styledBackButton.ts
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import styled from "styled-components";
 
 export const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap:8px;
+  gap: 8px;
   font-weight: 500;
   font-size: 14px;
   line-height: 150%;
@@ -13,4 +13,8 @@ export const Button = styled.button`
   border: none;
   cursor: pointer;
   color: var(--secondary-text);
+
+  &:hover {
+    text-transform: uppercase;
+  }
 `;
