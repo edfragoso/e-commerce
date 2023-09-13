2965e512991a538ab5adfd87f2d784f165587e2e
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-13 15:06:43 -0300

    feat: add navigate router to "/cart"

--- e-commerce/src/components/cart-control.tsx
+++ e-commerce/src/components/cart-control.tsx
@@ -1,11 +1,17 @@
 import { useLocalStorage } from "@/hooks/useLocalstorage";
 import { CartIcon } from "./icons/cart-icon";
 import { CartCount, Container } from "./Styles/styledCartControl";
+import { useRouter } from "next/navigation";
 
 export function CartControl() {
-    const { value } = useLocalStorage("cart-items", []); 
+    const { value } = useLocalStorage("cart-items", []);
+    const router = useRouter();
+    
+    const handleNavigateToCart = () => {
+      router.push('/cart')
+    } 
     return (
-      <Container>
+      <Container onClick={handleNavigateToCart}>
         <CartIcon />
         {value.length > 0 && <CartCount>{value.length}</CartCount>} 
       </Container>
