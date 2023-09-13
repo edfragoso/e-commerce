bbfa02112155c6ad0c253274132e6ab21adc2fb6
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-09-13 15:18:00 -0300

    feat: implement navigate to "/products:id"

--- e-commerce/src/components/ProductCard.tsx
+++ e-commerce/src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { formatValue } from "@/utils/formatPrice";
 import { Card } from "./Styles/styledProducCard";
 import { useRouter } from "next/navigation";
+import { DividerSpace } from "./divider";
 
 interface ProductCardProps {
   image: string;
@@ -10,18 +11,18 @@ interface ProductCardProps {
 }
 
 export function ProductCard(props: ProductCardProps) {
-    const price = formatValue(props.price)
-    const router = useRouter();
+  const price = formatValue(props.price);
+  const router = useRouter();
 
-    const handleNavigate = () => {
-      router.push("/products?id=" + props.id)
-    } 
+  const handleNavigate = () => {
+    router.push("/products?id=" + props.id);
+  };
   return (
     <Card onClick={handleNavigate}>
       <img src={props.image} />
       <div>
         <h3>{props.title}</h3>
-        <div></div>
+        <DividerSpace />
         <p>{price}</p>
       </div>
     </Card>
